45237d6b58c05ca3996939b6913c160b2d93a90d
spark-156 <57094525+spark-156@users.noreply.github.com>
2020-02-20 10:59:16 +0100

    meer datavelden toevoegen aan backend
    was heel weinig werk. Heb verder mijn tijd besteed aan bronnen over user stories lezen

--- python/backend.py
+++ python/backend.py
@@ -8,9 +8,21 @@ CORS(app)
 
 @app.route('/toevoegen', methods=['POST'])
 def voeg_kennisbron_toe():
+    """"Voegt kennisbron toe in de database"""
     data = request.json
     print(data)
-    db.execute_sql("INSERT INTO kennisbron(what, why, how) VALUES ('{}','{}','{}')".format(data['what'], data['why'], data['how']))
+    #Makkelijk om te editen, verander als je wilt
+    db.execute_sql(f"""INSERT INTO kennisbron(what, why, how, voorbeeld, 1, 2, 3, 4, 5) 
+                     VALUES 
+                     {data['what']},
+                     {data['why']}, 
+                     {data['how']}, 
+                     {data['voorbeeld']},
+                     {data['1']},
+                     {data['2']},
+                     {data['3']},
+                     {data['4']},
+                     {data['5']}""")
     return jsonify({'success': True}), 200, {'ContentType': 'application/json'}
 
 def check_input():
@@ -18,14 +30,22 @@ def check_input():
 
 @app.route('/ophalen', methods=['GET'])
 def vraag_kennisbron_op():
+    """Returnd alle velden van kennisbron waaronder What Why How"""
     billydb = db.execute_sql('SELECT * FROM kennisbron')
 
-
     kennisbronnen = []
-    for x in billydb:
+    for kennisbron in billydb:
         kennisbronnen.append(
-                {'what':x['what'], 'why':x['why'], 'how':x['how']})
-
+                {'what':kennisbron['what'],
+                 'why':kennisbron['why'],
+                 'how':kennisbron['how'],
+                 'voorbeeld':kennisbron['voorbeeld'],
+                 # '': kennisbron[''],
+                 # '': kennisbron[''],
+                 # '': kennisbron[''],
+                 # '': kennisbron[''],
+                 # '': kennisbron['']
+                 })
     return jsonify(kennisbronnen), 200, {'ContentType': 'application/json'}
 
-app.run(host='0.0.0.0')
+app.run(host='0.0.0.0') #run host op LAN
