7895656a48dab6011a516d2091814679a1ddddf5
Faisal963 <faisal.alhag@student.hu.nl>
2020-02-21 16:33:09 +0100

    css en script aanpassingen
    ""fuck life""
    
    nieuwe velden toegevoegd aan de html
    die zijn ook verwerkt in de script file

--- Frontend/javascript/script.js
+++ Frontend/javascript/script.js
@@ -1,15 +1,20 @@
-function kenniskaart_toevoegen() {
+function kennisbron_toevoegen() {
 
-	let awhat = document.getElementById('myWhat').value;
-	let awhy = document.getElementById('myWhy').value;
-	let ahow = document.getElementById('myHow').value;
+	var what = document.getElementById('myWhat').value;
+	var why = document.getElementById('myWhy').value;
+	var how = document.getElementById('myHow').value;
+	var titel = document.getElementById('myTitel').value;
+	var hboi = document.getElementById('myHboi').value;
+	var rol = document.getElementById('myRol').value;
+	var voorbeeld = document.getElementById('myVoorbeeld').value;
+	var vaardigheid = document.getElementById('myVaardigheid').value;
 
 			
-	let data = {"what": awhat, "why": awhy, "how": ahow};
+	var data = {"what": what, "why": why, "how": how, "titel":titel ,"hboi":hboi ,"rol":rol ,"voorbeeld":voorbeeld ,"vaardigheid":vaardigheid };
 
 	alert(JSON.stringify(data));			
 
-	fetch('http://192.168.3.72:5000/toevoegen', {
+	fetch('http://192.168.3.73:5000/toevoegen', {
 		method: 'POST',
 		headers: {'Accept': 'application/json', 'Content-Type': 'application/json'},
 		body: JSON.stringify(data)
