ae04188faaf56cacdcbf0f5764249965853dbd0b
Faisal963 <faisal.alhag@student.hu.nl>
2020-02-21 16:46:14 +0100

    Revert "css en script aanpassingen"
    This reverts commit 7895656a48dab6011a516d2091814679a1ddddf5.

--- Frontend/javascript/script.js
+++ Frontend/javascript/script.js
@@ -1,20 +1,15 @@
-function kennisbron_toevoegen() {
+function kenniskaart_toevoegen() {
 
-	var what = document.getElementById('myWhat').value;
-	var why = document.getElementById('myWhy').value;
-	var how = document.getElementById('myHow').value;
-	var titel = document.getElementById('myTitel').value;
-	var hboi = document.getElementById('myHboi').value;
-	var rol = document.getElementById('myRol').value;
-	var voorbeeld = document.getElementById('myVoorbeeld').value;
-	var vaardigheid = document.getElementById('myVaardigheid').value;
+	let awhat = document.getElementById('myWhat').value;
+	let awhy = document.getElementById('myWhy').value;
+	let ahow = document.getElementById('myHow').value;
 
 			
-	var data = {"what": what, "why": why, "how": how, "titel":titel ,"hboi":hboi ,"rol":rol ,"voorbeeld":voorbeeld ,"vaardigheid":vaardigheid };
+	let data = {"what": awhat, "why": awhy, "how": ahow};
 
 	alert(JSON.stringify(data));			
 
-	fetch('http://192.168.3.73:5000/toevoegen', {
+	fetch('http://192.168.3.72:5000/toevoegen', {
 		method: 'POST',
 		headers: {'Accept': 'application/json', 'Content-Type': 'application/json'},
 		body: JSON.stringify(data)
