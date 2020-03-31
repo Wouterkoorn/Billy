9069eae392c34aaad8a9ff51a36074c72d5fc4d1
spark-156 <lucabergman@hotmail.nl>
2020-03-31 11:12:40 +0200

    Update zoekresultaten.js

--- Frontend/javascript/zoekresultaten.js
+++ Frontend/javascript/zoekresultaten.js
@@ -1,89 +1,9 @@
-// var what = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean commodo at orci in hendrerit. Donec et varius nisi, vitae mattis elit. Sed rhoncus odio eget nisl aliquam dignissim. Donec ac auctor dui. Nullam in libero ut ligula gravida rhoncus. Pellentesque rutrum fringilla dictum. Donec imperdiet dictum justo, et hendrerit nisl pharetra eu. Etiam a dignissim libero, consectetur egestas lacus. Curabitur fermentum nisl odio, a sodales turpis pulvinar ac. Mauris lacinia, nibh ut bibendum faucibus, purus odio blandit sapien, eu malesuada velit urna id tortor. Maecenas congue, ex eget malesuada sodales, tellus neque ultrices diam, in consectetur metus quam quis sem. Fusce sodales tempus magna, id varius leo vehicula tempus. Nulla mattis nisi id congue vestibulum. Mauris accumsan viverra urna, ut vulputate dui semper non."
-var testSearchResults =[
-  {
-    "kenniskaart_ID": 0,
-    "titel": "Lerom ipsum",
-    "what": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer mattis lorem sed lectus euismod sagittis in at augue. Maecenas tristique orci vel tellus fringilla elementum. Nullam a elementum magna. Aliquam aliquet purus lobortis ultricies egestas. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut sapien ex, imperdiet eu vestibulum id, pellentesque in ligula. Nullam vel neque pretium, mattis lacus non, ultrices sapien. Curabitur faucibus vehicula enim, ac mattis nisi congue ut. Nullam tempus orci vel urna finibus dictum. Aliquam mollis malesuada iaculis. Mauris at felis nec quam ultricies.",
-    "why": "test",
-    "how": "test",
-    "voorbeeld": "test",
-    "rol": "test",
-    "vaardigheid": "test",
-    "hboi": "test",
-    "datum": "12 maart 2020",
-    "tags": ["hoi11", "hoi22", "hoi33", "hoi44"],
-  },
-  {
-    "kenniskaart_ID": 5,
-    "titel": "Lerom ipsum 5",
-    "what": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer mattis lorem sed lectus euismod sagittis in at augue. Maecenas tristique orci vel tellus fringilla elementum. Nullam a elementum magna. Aliquam aliquet purus lobortis ultricies egestas. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut sapien ex, imperdiet eu vestibulum id, pellentesque in ligula. Nullam vel neque pretium, mattis lacus non, ultrices sapien. Curabitur faucibus vehicula enim, ac mattis nisi congue ut. Nullam tempus orci vel urna finibus dictum. Aliquam mollis malesuada iaculis. Mauris at felis nec quam ultricies.",
-    "why": "test",
-    "how": "test",
-    "voorbeeld": "test",
-    "rol": "test",
-    "vaardigheid": "test",
-    "hboi": "test",
-    "datum": "13 maart 2020",
-    "tags": ["hoi1", "hoi2", "hoi3", "hoi4"],
-  },
-    {
-    "kenniskaart_ID": 0,
-    "titel": "Lerom ipsum",
-    "what": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer mattis lorem sed lectus euismod sagittis in at augue. Maecenas tristique orci vel tellus fringilla elementum. Nullam a elementum magna. Aliquam aliquet purus lobortis ultricies egestas. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut sapien ex, imperdiet eu vestibulum id, pellentesque in ligula. Nullam vel neque pretium, mattis lacus non, ultrices sapien. Curabitur faucibus vehicula enim, ac mattis nisi congue ut. Nullam tempus orci vel urna finibus dictum. Aliquam mollis malesuada iaculis. Mauris at felis nec quam ultricies.",
-    "why": "test",
-    "how": "test",
-    "voorbeeld": "test",
-    "rol": "test",
-    "vaardigheid": "test",
-    "hboi": "test",
-    "datum": "12 maart 2020",
-    "tags": ["hoi11", "hoi22", "hoi33", "hoi44"],
-  },
-  {
-    "kenniskaart_ID": 5,
-    "titel": "Lerom ipsum 5",
-    "what": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer mattis lorem sed lectus euismod sagittis in at augue. Maecenas tristique orci vel tellus fringilla elementum. Nullam a elementum magna. Aliquam aliquet purus lobortis ultricies egestas. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut sapien ex, imperdiet eu vestibulum id, pellentesque in ligula. Nullam vel neque pretium, mattis lacus non, ultrices sapien. Curabitur faucibus vehicula enim, ac mattis nisi congue ut. Nullam tempus orci vel urna finibus dictum. Aliquam mollis malesuada iaculis. Mauris at felis nec quam ultricies.",
-    "why": "test",
-    "how": "test",
-    "voorbeeld": "test",
-    "rol": "test",
-    "vaardigheid": "test",
-    "hboi": "test",
-    "datum": "13 maart 2020",
-    "tags": ["hoi1", "hoi2", "hoi3", "hoi4"],
-  },
-    {
-    "kenniskaart_ID": 0,
-    "titel": "Lerom ipsum",
-    "what": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer mattis lorem sed lectus euismod sagittis in at augue. Maecenas tristique orci vel tellus fringilla elementum. Nullam a elementum magna. Aliquam aliquet purus lobortis ultricies egestas. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut sapien ex, imperdiet eu vestibulum id, pellentesque in ligula. Nullam vel neque pretium, mattis lacus non, ultrices sapien. Curabitur faucibus vehicula enim, ac mattis nisi congue ut. Nullam tempus orci vel urna finibus dictum. Aliquam mollis malesuada iaculis. Mauris at felis nec quam ultricies.",
-    "why": "test",
-    "how": "test",
-    "voorbeeld": "test",
-    "rol": "test",
-    "vaardigheid": "test",
-    "hboi": "test",
-    "datum": "12 maart 2020",
-    "tags": ["hoi11", "hoi22", "hoi33", "hoi44"],
-  },
-  {
-    "kenniskaart_ID": 5,
-    "titel": "Lerom ipsum 5",
-    "what": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer mattis lorem sed lectus euismod sagittis in at augue. Maecenas tristique orci vel tellus fringilla elementum. Nullam a elementum magna. Aliquam aliquet purus lobortis ultricies egestas. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut sapien ex, imperdiet eu vestibulum id, pellentesque in ligula. Nullam vel neque pretium, mattis lacus non, ultrices sapien. Curabitur faucibus vehicula enim, ac mattis nisi congue ut. Nullam tempus orci vel urna finibus dictum. Aliquam mollis malesuada iaculis. Mauris at felis nec quam ultricies.",
-    "why": "test",
-    "how": "test",
-    "voorbeeld": "test",
-    "rol": "test",
-    "vaardigheid": "test",
-    "hboi": "test",
-    "datum": "13 maart 2020",
-    "tags": ["hoi1", "hoi2", "hoi3", "hoi4"],
-  },
-];
 
 function formatDateTime(unformatedDatum) {
   var datum = new Date(unformatedDatum);
   return datum.getDate() + " " + datum.getMonth() + " " + datum.getFullYear()
 }
+
 
 function makeElement(locatie, classnaam, contentInElement, ElementType) {
   var element = document.createElement(ElementType);
@@ -92,12 +12,14 @@ function makeElement(locatie, classnaam, contentInElement, ElementType) {
   locatie.appendChild(element);
 }
 
+
 function makeImg(locatie, classnaam, imageInDiv) {
   var img = document.createElement('img');
   img.setAttribute('src', imageInDiv); //verander value naar imageInDiv
   img.setAttribute('class', classnaam);
   locatie.appendChild(img);
 }
+
 
 function toonZoekResultaten(item, index) {
   var kenniskaartBestemming = document.getElementsByClassName("kenniskaartencontainer");
@@ -115,6 +37,7 @@ function toonZoekResultaten(item, index) {
   //   makeElement(kenniskaartlocatie[index], "kenniskaart-tags", item, "div")
   // });
 }
+
 
 // testSearchResults.forEach(function(item, index) {toonZoekResultaten(item, index)})
 
