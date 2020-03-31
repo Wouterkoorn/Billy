07aec3e23cf0ff556dd4bd9252705ea9864e317e
Wouterkoorn <54982288+Wouterkoorn@users.noreply.github.com>
2020-03-31 22:04:47 +0200

    what why how buffer vergroot

--- Backend/sql/kenniskaarten.sql
+++ Backend/sql/kenniskaarten.sql
@@ -3,9 +3,9 @@ DROP TABLE IF EXISTS kenniskaarten;
 CREATE TABLE `kenniskaarten` (
   `kenniskaart_ID` Integer(10) NOT NULL AUTO_INCREMENT,
   `titel` Varchar(255),
-  `what` Varchar(255),
-  `why` Varchar(255),
-  `how` Varchar(255),
+  `what` Varchar(1023),
+  `why` Varchar(1023),
+  `how` Varchar(1023),
   `voorbeeld` Varchar(255),
   `rol` Varchar(255),
   `vaardigheid` Varchar(255),
