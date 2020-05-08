5f1b84d52adf53f8e1b0e4580879b285b5434fba
spark-156 <lucabergman@hotmail.nl>
2020-05-08 16:59:54 +0200

    Kenniskaart popup mvp

--- Frontend/javascript/popup.js
+++ Frontend/javascript/popup.js
@@ -1,4 +1,5 @@
 function addEventListeners() {
+    let popup = document.getElementById("popupContainer")
     let kenniskaarten = document.getElementsByClassName("kenniskaart");
     //gaat alle kenniskaarten langs en doet voor ieder:
     let i;
@@ -17,14 +18,31 @@ function addEventListeners() {
                 .then(function (response) {
                     response.json().then( function (data) {
                         console.log(data);
-                        //dynamisch maken van pop-up
-
+                        console.log(data["datetime"], formatDateTime(data["datetime"]));
+                        //pop-up vullen met juiste data
+                        document.getElementsByClassName("popupContent")[0].setAttribute("id", data["id"])
+                        document.getElementById("popupTitel").innerHTML = data["titel"]
+                        document.getElementById("popupDatum").innerHTML = formatDateTime(data["datetime"])
+                        document.getElementById("popupHBO-i").innerHTML = data["hboi"]
+                        document.getElementById("popupVaardigheid").innerHTML = data["vaardigheid"]
+                        document.getElementById("popupRol").innerHTML = data["rol"]
+                        document.getElementById("popupWhat").innerHTML = data["what"]
+                        document.getElementById("popupWhy").innerHTML = data["why"]
+                        document.getElementById("popupHow").innerHTML = data["why"]
+                        document.getElementById("popupVoorbeeld").innerHTML = data["voorbeeld"]
+                        //pop-up tonen
+                        popup.style.display = "block";
+                        popup.addEventListener("click", function (element) {
+                            //naast popup clicken sluit de pop-up
+                            if (element.target.getAttribute("id") === popup.getAttribute("id")) {
+                                popup.style.display = "none";
+                            }
+                        })
                     })
                 })
+
         })
     }
-
-
 }
 
 
