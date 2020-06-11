b7804852f4bbeaaa0807a456607a02b11688fe1d
wouter <wouter.koorn@student.hu.nl>
2020-06-11 14:07:35 +0200

    doet het

--- nginx/frontend/javascript/toevoegen.js
+++ nginx/frontend/javascript/toevoegen.js
@@ -39,7 +39,7 @@ function kenniskaart_toevoegen() {
         "why": document.getElementById("formWhy").value,
         "how": document.getElementById("formHow").value,
         "rollen": lijstRollen,
-        "competentie": lijstCompetenties,
+        "competenties": lijstCompetenties,
         "hboi": lijsthboi,
         "voorbeeld": document.getElementById('formVoorbeeld').value,
         "bronnen": document.getElementById("formBronnen").value
