ede478e4f39fcc903a0a55ee3722d8c49058c77a
Faisal963 <faisal.alhag@student.hu.nl>
2020-06-19 18:42:48 +0200

    active event for homepage

--- nginx/frontend/javascript/startup.js
+++ nginx/frontend/javascript/startup.js
@@ -1,2 +1,18 @@
 
 urlParserIndex();
+
+// Add active class to the current nav elemnet (highlight it)
+var header = document.getElementById('menu');
+var pages = header.getElementsByClassName('toevoegL');
+
+for (var i = 0; i < pages.length; i++) {
+  pages[i].addEventListener('onclick', function() {
+    alert(document.className.innerHTML)
+
+    var huidig = document.getElementsByClassName('active');
+    if (huidig.length > 0) {
+      huidig[0].className = huidig[0].className.replace(' active', 'active');
+    }
+    huidig.className += ' active';
+  });
+}
