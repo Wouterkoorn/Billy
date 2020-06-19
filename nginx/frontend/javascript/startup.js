5884d3e9e0befa99f123e121ec55df807e2ecce6
Faisal963 <faisal.alhag@student.hu.nl>
2020-06-19 19:07:54 +0200

    Gefixt nav menu zonder JS

--- nginx/frontend/javascript/startup.js
+++ nginx/frontend/javascript/startup.js
@@ -1,18 +1,2 @@
 
 urlParserIndex();
-
-// Add active class to the current nav elemnet (highlight it)
-var header = document.getElementById('menu');
-var pages = header.getElementsByClassName('toevoegL');
-
-for (var i = 0; i < pages.length; i++) {
-  pages[i].addEventListener('onclick', function() {
-    alert('am hier')
-
-    var huidig = document.getElementsByClassName('active');
-    if (huidig.length > 0) {
-      huidig[0].className = huidig[0].className.replace(' active', 'active');
-    }
-    huidig.className += ' active';
-  });
-}
